fe7c0326930d0b4bd4b154e1eee600bd4d38fcc6
Brian Klemm <brian@bigsigma.tech>
2026-01-26 09:45:30 -0800

    Remove stale mock_sgen_engine import from API init

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -4,12 +4,10 @@ User API Functions and Requests
 
 from .health import health_check
 from .sgen_submit import (
-    mock_sgen_engine,
-    submit_job,
+    submit_job
 )
 
 __all__ = [
     "health_check",
-    "mock_sgen_engine",
     "submit_job",
 ]
